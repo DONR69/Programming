39e72cd6846de68af6bdcac35d977458d83958c0
Daniil <02dandy02@mail.ru>
2021-02-04 00:50:16 +0300

    up to tenth

---
+++ Practice/04/Python/04.py
@@ -0,0 +1,18 @@
+print('Введите 1 число')
+a = int(input())
+print("Введите 2 число")
+b = int(input())
+c = a
+a = b
+b = c
+print("Значение 1 числа", a)
+print("Значение 2 числа", b)
+print('Введите 1 число')
+a = int(input())
+print("Введите 2 число")
+b = int(input())
+a = b - a
+b = b - a
+a = b + a
+print("Значение 1 числа", a)
+print("Значение 2 числа", b)
