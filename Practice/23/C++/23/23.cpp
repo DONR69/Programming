efeeb825002dd7fd69bdf55f00ea44fb4884b50b
Daniil <02dandy02@mail.ru>
2021-02-10 17:58:42 +0300

    added 32 some still remain

---
+++ Practice/23/C++/23/23.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "Fac.h" 
+#include "Sin.h" 
+#include "Soch.h"
+using namespace std;
+int main() {
+	cout << "n" << "\t" << "n!\n";
+	for (int n = 1; n <= 10; n++) {
+		cout << n << "\t" << Fac(n) << "\n";
+	}
+	cout << "\n";
+
+	float p = 3.1415;
+	cout << "x" << "\t" << "sin(x)\n";
+	for (float x = 0; x <= p / 4; x = x + p / 180) {
+		cout << x << "\t" << setprecision(4) << Sin(x) << "\n";
+	}
+	cout << "\n";
+
+	int n = 10;
+	cout << "k" << "\t" << "c(k, 10)\n";
+	for (int k = 1; k <= n; k++) {
+		cout << k << "\t" << Soch(k, n) << "\n";
+	}
+}
